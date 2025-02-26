9eea25349a3515de0a9c82a38594b05e1699796d
mateushmd <mateushmdiniz@gmail.com>
2025-02-26 15:12:38 -0300

    working main menu

--- main.pas
+++ main.pas
@@ -1,6 +1,9 @@
 program main;
 
-uses SDL2;
+uses SysUtils, SDL2, SDL2_ttf;
+
+type
+    windowState_t = (start, game);
 
 const
     WINDOW_TITLE    = 'Pascal Pong';
@@ -10,18 +13,29 @@ const
     PLAYER_WIDTH    = 15;
     PLAYER_HEIGHT   = 150;
     PLAYER_SPEED    = 5;
+    MAX_TEXTS = 3;
 
 var
     sdlEvent: PSDL_Event;
     Running: Boolean;
     window: PSDL_Window;
     renderer: PSDL_Renderer;
+    font: PTTF_Font;
+    textSurface: PSDL_Surface;
+    textTextures: array[0..MAX_TEXTS - 1] of PSDL_Texture;
+    textColor: TSDL_Color;
+    textRects: array[0..MAX_TEXTS - 1] of TSDL_Rect;
+    textsVisible: array[0..MAX_TEXTS - 1] of boolean; 
     keyboardState: PUInt8;
     player1Rect: TSDL_Rect;
     player2Rect: TSDL_Rect;
     border1Rect: TSDL_Rect;
     border2Rect: TSDL_Rect;
     playerMiddle: integer = WINDOW_HEIGHT div 2 - PLAYER_HEIGHT div 2;
+    state: windowState_t = start;
+    lowerOption: boolean = false;
+    lock: boolean = false;
+    i: integer;
 
 procedure setRectangle(var rect: TSDL_Rect; width, height, xPos, yPos: integer);
 begin
@@ -31,14 +45,158 @@ begin
     rect.y := yPos;
 end;
 
+procedure updateText(index: integer; text: string; xPos, yPos: integer; center: boolean);
+begin
+    if textTextures[index] <> nil then
+        SDL_DestroyTexture(textTextures[index]);
+
+    textSurface := TTF_RenderText_Solid(font, PChar(AnsiString(text)), textColor);
+
+    textTextures[index] := SDL_CreateTextureFromSurface(renderer, textSurface);
+    
+    SDL_FreeSurface(textSurface);
+
+    SDL_QueryTexture(textTextures[index], nil, nil, @textRects[index].w, @textRects[index].h);
+
+    textRects[index].x := xPos;
+    textRects[index].y := yPos;
+
+    if center then
+    begin
+        textRects[index].x := textRects[index].x - textRects[index].w div 2;
+        textRects[index].y := textRects[index].y - textRects[index].h div 2;
+    end;
+end;
+
+procedure update();
+begin
+    new(sdlEvent);
+    while SDL_PollEvent(sdlEvent) <> 0 do
+    begin
+        case sdlEvent^.type_ of
+            SDL_QUITEV: 
+                Running := False;
+
+            SDL_KEYUP:
+                lock := false;
+        end;
+    end;
+
+    SDL_PumpEvents;
+    
+    // EXIT BY PRESSING ESC
+    if keyboardState[SDL_SCANCODE_ESCAPE] = 1 then
+        Running := False;
+
+    // W S
+    if (state = game) and (keyboardState[SDL_SCANCODE_W] = 1) then
+        player1Rect.y := player1Rect.y - PLAYER_SPEED;
+    if (state = game) and (keyboardState[SDL_SCANCODE_S] = 1) then
+        player1Rect.y := player1Rect.y + PLAYER_SPEED;
+
+    // ARROW_UP ARROW_DOWN
+    if keyboardState[SDL_SCANCODE_UP] = 1 then
+    begin
+        if state = start then
+        begin
+            if not lock then
+            begin
+                lowerOption := false;
+                if lowerOption then
+                    updateText(2, '>', WINDOW_WIDTH div 2 - 300, WINDOW_HEIGHT div 2 + 80, true)
+                else
+                    updateText(2, '>', WINDOW_WIDTH div 2 - 300, WINDOW_HEIGHT div 2 - 80, true);
+
+                lock := true;
+            end;
+        end
+        else
+            player2Rect.y := player2Rect.y - PLAYER_SPEED;
+    end;
+
+
+    if keyboardState[SDL_SCANCODE_DOWN] = 1 then
+    begin
+        if state = start then
+        begin
+            if not lock then
+            begin
+                lowerOption := true;
+                if lowerOption then
+                    updateText(2, '>', WINDOW_WIDTH div 2 - 300, WINDOW_HEIGHT div 2 + 80, true)
+                else
+                    updateText(2, '>', WINDOW_WIDTH div 2 - 300, WINDOW_HEIGHT div 2 - 80, true);
+
+                lock := true;
+            end;
+        end
+        else
+            player2Rect.y := player2Rect.y + PLAYER_SPEED;
+    end;
+end;
+
+procedure renderStart();
+begin
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+end;
+
+procedure renderGame();
+begin
+    // RECT 
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_RenderFillRect(renderer, @player1Rect);
+    SDL_RenderFillRect(renderer, @player2Rect);
+
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderFillRect(renderer, @border1Rect);
+    SDL_RenderFillRect(renderer, @border2Rect);
+end;
+
+procedure render();
+begin
+    // BACKGROUND
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
+    case state of
+        start: renderStart();
+        game: renderGame();
+    end;
+
+    for i := 0 to MAX_TEXTS - 1 do
+    begin
+        if textsVisible[i] = true then
+            SDL_RenderCopy(renderer, textTextures[i], nil, @textRects[i]);
+    end;
+    
+    SDL_RenderPresent(renderer);
+    SDL_Delay(16);
+end;
+
 begin
     if SDL_Init(SDL_INIT_VIDEO) < 0 then Exit;
+    if TTF_Init() < 0 then Exit;
         
     window := SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     if window = nil then Halt;
         
     renderer := SDL_CreateRenderer(window, -1, 0);
     if renderer = nil then Halt;
+
+    font := TTF_OpenFont('./res/PixelatedEleganceRegular-ovyAA.ttf', 50);
+    if font = nil then Halt;
+
+    textColor.r := 255;
+    textColor.g := 255;
+    textColor.b := 255;
+    textColor.a := 255;
+
+    for i := 0 to MAX_TEXTS - 1 do
+        textsVisible[i] := true;
+
+    updateText(0, 'Um jogador', WINDOW_WIDTH div 2, WINDOW_HEIGHT div 2 - 80, true);
+    updateText(1, 'Dois jogadores', WINDOW_WIDTH div 2, WINDOW_HEIGHT div 2 + 80, true);
+    updateText(2, '>', WINDOW_WIDTH div 2 - 300, WINDOW_HEIGHT div 2 - 80, true);
 
     setRectangle(player1Rect, PLAYER_WIDTH, PLAYER_HEIGHT, 0 + OFFSET_BORDER, playerMiddle);
     setRectangle(player2Rect, PLAYER_WIDTH, PLAYER_HEIGHT, WINDOW_WIDTH - OFFSET_BORDER, playerMiddle);
@@ -49,50 +207,15 @@ begin
     Running := True;
     while Running do
     begin
-        new(sdlEvent);
-        while SDL_PollEvent(sdlEvent) <> 0 do
-        begin
-            case sdlEvent^.type_ of
-                SDL_QUITEV: 
-                    Running := False;
-            end;
-        end;
-
-        SDL_PumpEvents;
-        
-        // EXIT BY PRESSING ESC
-        if keyboardState[SDL_SCANCODE_ESCAPE] = 1 then
-        Running := False;
-
-        // W S
-        if keyboardState[SDL_SCANCODE_W] = 1 then
-            player1Rect.y := player1Rect.y - PLAYER_SPEED;
-        if keyboardState[SDL_SCANCODE_S] = 1 then
-            player1Rect.y := player1Rect.y + PLAYER_SPEED;
-
-        // ARROW_UP ARROW_DOWN
-        if keyboardState[SDL_SCANCODE_UP] = 1 then
-            player2Rect.y := player2Rect.y - PLAYER_SPEED;
-        if keyboardState[SDL_SCANCODE_DOWN] = 1 then
-            player2Rect.y := player2Rect.y + PLAYER_SPEED;
-
-        // BACKGROUND
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-        SDL_RenderClear(renderer);
-
-        // RECT
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-        SDL_RenderFillRect(renderer, @player1Rect);
-        SDL_RenderFillRect(renderer, @player2Rect);
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
-        SDL_RenderFillRect(renderer, @border1Rect);
-        SDL_RenderFillRect(renderer, @border2Rect);
-
-        SDL_RenderPresent(renderer);
-        SDL_Delay(16);
+        update();
+        render();
     end;
-    
+
+    for i := 0 to MAX_TEXTS - 1 do    
+        SDL_DestroyTexture(textTextures[i]);
+    TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_Quit;
     SDL_Quit;
 end.
